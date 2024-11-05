f2e9f887597e4d79b22095dada5947aa3b6b27cc
qwerty4967 <jmcraft126@gmail.com>
2024-11-05 11:39:15 -0500

    1bpp

--- charsetGen.py
+++ charsetGen.py
@@ -18,24 +18,29 @@ def letter_toHex(letter:int, desc:str):
     return toReturn
 
 def line_toHex(image:Image, line:int):
-    toReturn = "word 0x"
-    for i in (0, 2, 4, 6):
+    toReturn = "0x"
+    for i in (0, 4):
         toReturn += pixelGroup_toChar(image, line, i)
     return toReturn
 
 def pixelGroup_toChar(image:Image, line:int, pos:int):
+    # yes, I'm aware all of this is terrible, but I'm very tired, and it will do.
     first  = image.getpixel((pos, line)) == 255
     second = image.getpixel((pos+1, line)) == 255
+    third = image.getpixel((pos+2, line))==255
+    fourth = image.getpixel((pos+3, line))==255
+    arr = [first, second, third, fourth]
 
-    if (not first) and not second:
-        return '0'
-    if (not first) and second:
-        return '3'
-    if first and not second:
-        return 'C'
-    return 'F'
+    val = 0
+    for bool in arr:
+        val *= 2
+        if bool:
+            val+=1
 
-arr = ["[END]", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M","N","O", "P", "Q", "R", "S", "T", "U", "V",
+    hexArr = [ '0', '1','2','3',"4","5","6","7","8","9","A", "B", "C", "D", "E", "F"]
+    return hexArr[val]
+
+arr = ["[END]", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M","N","O", "P", "Q", "R", "S", "T", "U", "V",
        "W", "X", "Y", "Z", "0","1","2","3","4","5","6","7","8","9", "[SPACE]", "[ENTER]", "[BACK]",".",",","!","?",":",
        "+", "-", "[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]",
        "[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]","[UNUSED]",
